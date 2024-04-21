a3a6ce1b4c54a5c6b385411f544cfa68cb3e13db
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-04-21 13:41:21 +0900

    feat: set my framework

---
+++ src/components/Products/Product.tsx
@@ -0,0 +1,11 @@
+import type { Product } from "@/db";
+
+const Product = ({ product }: { product: Product }) => {
+  return (
+    <div className="group relative">
+      <div className="aspect-h-1"></div>
+    </div>
+  );
+};
+
+export default Product;
